90b0f8e9e0c663b4aeef77c6f21c2341135de3f6
Mederic Carriat <carriatmederic@gmail.com>
2021-12-13 17:31:11 +0100

    Start adding raytracing logic

--- src/shaders/pathtrace.comp.glsl
+++ src/shaders/pathtrace.comp.glsl
@@ -10,6 +10,84 @@ struct Ray
   vec3 dir;
 };
 
+struct Sphere
+{
+  vec3 center;
+  float radius;
+};
+
+struct Collision
+{
+  float t;              // distance along ray
+  vec3 p;               // world position of collision
+  vec3 n;               // surface normal at collision
+  bool inside;          // ray inside the object or not
+  int object_index;     // index of the object
+};
+
+
+// Adapted from scratchapixel's sphere intersection 
+Collision collision(Sphere sphere, Ray ray)
+{
+  Collision c;
+  c.inside = false;
+
+  vec3 oc = sphere.center - ray.origin;
+  float t_ca = dot(oc, ray.dir);
+  float d2 = dot(oc, oc) - t_ca;
+
+  // Ray goes outside of sphere
+  if (d2 > sphere.radius) 
+  {
+    c.t = -1;
+    return c;
+  }
+
+  float t_hc = sqrt(sphere.radius*sphere.radius - d2);
+  float t0 = t_ca - t_hc;
+  float t1 = t_ca + t_hc;
+  float t_near = min(t0, t1);
+  float t_far = max(t0, t1);
+  c.t = t_near;
+
+  // Sphere behind the ray, no intersection
+  if (t_far < 0.0)
+  {
+    c.t = -1.0;
+    return c;
+  }
+
+  // Ray inside the sphere
+  if (t_near < 0.0)
+  {
+    c.t = t_far;
+    c.inside = true;
+  }
+
+  c.p = ray.origin + ray.dir * t0; 
+  c.n = normalize(c.p - sphere.center);
+
+  // Flip normal if ray inside
+  if (c.inside) c.n *= 1.0;
+
+  return c;
+}
+
+vec3 raytrace(Ray ray)
+{
+  Sphere sphere;
+  sphere.center = vec3(0, 0, 3);
+  sphere.radius = 1.0;
+
+  Collision c = collision(sphere, ray);
+  if (c.t > 0.0)
+  {
+    return vec3(1, 0, 0);
+  }
+  
+  return vec3(0, 0, 0);
+}
+
 
 void main()
 {
@@ -28,7 +106,7 @@ void main()
   ray.dir = normalize(pixel_world - ray.origin);
   
   // Cast the ray out into the world and intersect the ray with objects
-  //vec3 color = raytrace(world_ray);
-  vec3 color = vec3(float(pixel.x) / 64, float(pixel.y) / 64, 1);
+  vec3 color = raytrace(ray);
+  //vec3 color = vec3(float(pixel.x) / 64, float(pixel.y) / 64, 1);
   imageStore(output_texture, pixel, vec4(color, 1.0));
 }
