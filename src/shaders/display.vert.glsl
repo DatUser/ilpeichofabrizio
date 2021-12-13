b0f0cfd34d5df5c485e0741cf57f70635535b633
Mederic Carriat <carriatmederic@gmail.com>
2021-12-13 15:31:58 +0100

    Add shaders for pathtracing

---
+++ src/shaders/display.vert.glsl
@@ -0,0 +1,12 @@
+#version 450
+
+layout (location = 0) in vec3 vert_pos;
+layout (location = 1) in vec2 vert_uv;
+
+out vec2 uv;
+
+void main()
+{
+  gl_Position = vec4(vert_pos, 1.0);
+  uv = vert_uv;
+}
